2f99b7ba6008e153d69b966105768e4530a1650f
mteresa-jimenez <mteresa.jn@gmail.com>
2020-11-12 09:14:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>Hola mundo</div>;
+  }
+}
+
+export default App;
